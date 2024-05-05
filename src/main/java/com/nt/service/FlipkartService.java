b40ffd76b23c4503f52b45b9dad7ac1ff8415cf0
unknown <jaiswalkraditya@gmail.com>
2024-05-05 19:22:32 +0530

    initila commit

---
+++ src/main/java/com/nt/service/FlipkartService.java
@@ -0,0 +1,16 @@
+package com.nt.service;
+
+import com.nt.util.StringUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class FlipkartService 
+{
+    public static void main( String[] args )
+    {
+        int tokenCount = StringUtil.StringCount("hello how r u");
+        System.out.println("word counts ::"+tokenCount);
+    }
+}
